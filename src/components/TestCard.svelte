5f32e5f00407e979ad54b77b95e4381e692c9e57
Kyle Olmstead <k.olmstead@offensive-security.com>
2023-12-27 10:53:31 -0500

    svelte post css test

--- src/components/TestCard.svelte
+++ src/components/TestCard.svelte
@@ -2,6 +2,22 @@
   let name = "Kyle";
 </script>
 
-{#if name}
-  <h3 class="sm:text-red-400 md:text-red-500">Hello {name}</h3>
-{/if}
+<!-- #svelte-test -->
+<div id="svelte-test">
+  {#if name}
+    <h3 class="sm:text-red-400 md:text-red-500">Hello {name}</h3>
+  {/if}
+
+  <div id="postcss-svelte-check" class="hidden">
+    Post css is working in svelte too
+  </div>
+</div>
+
+<!-- /#svelte-test -->
+
+<style lang="postcss">
+  #postcss-svelte-check {
+    @apply text-emerald-300;
+    display: block;
+  }
+</style>
