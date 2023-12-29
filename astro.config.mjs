446107ab4fc52c7afdf7b6323c345f610dd5d1eb
Kyle Olmstead <k.olmstead@offensive-security.com>
2023-12-29 11:05:03 -0500

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    svelte(),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-  ],
+	integrations: [
+		svelte(),
+		tailwind({
+			// Example: Disable injecting a basic `base.css` import on every page.
+			// Useful if you need to define and/or import your own custom `base.css`.
+			applyBaseStyles: false,
+		}),
+	],
 });
--- src/components/TestCard.svelte
+++ src/components/TestCard.svelte
@@ -1,23 +1,23 @@
 <script>
-  let name = "John Doe";
+	let name = 'John Doe';
 </script>
 
 <!-- #svelte-test -->
 <div id="svelte-test">
-  {#if name}
-    <h3 class="sm:text-red-400 md:text-red-500">Hello {name}</h3>
-  {/if}
+	{#if name}
+		<h3 class="sm:text-red-400 md:text-red-500">Hello {name}</h3>
+	{/if}
 
-  <div id="postcss-svelte-check" class="hidden">
-    Post css is working in svelte too
-  </div>
+	<div id="postcss-svelte-check" class="hidden">
+		Post css is working in svelte too
+	</div>
 </div>
 
 <!-- /#svelte-test -->
 
 <style lang="postcss">
-  #postcss-svelte-check {
-    @apply text-emerald-300  sm:text-emerald-400 lg:text-emerald-900;
-    display: block;
-  }
+	#postcss-svelte-check {
+		@apply text-emerald-300  sm:text-emerald-400 lg:text-emerald-900;
+		display: block;
+	}
 </style>
